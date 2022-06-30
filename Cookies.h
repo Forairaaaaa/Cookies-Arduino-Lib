70eaf56fcbd8be5563dabb97baa5cf2b6f1a0df3
Forairaaaaa <applesyqd@outlook.com>
2022-06-30 22:28:50 +0800

    add fastled

--- Cookies.h
+++ Cookies.h
@@ -1,10 +1,18 @@
 #ifndef _COOKIES_H_
 #define _COOKIES_H_
 
-#include "./FastLED/src/FastLED.h"
+#include "Arduino.h"
+#include "Wire.h"
+/* Sub lib */
 #include "./RGB-SEN/RGB_SEN.h"
 #include "./TinyMotor/TinyMotor.h"
 #include "./TinyPMU/TinyPMU.h"
 
+/* Cookies class */
+class COOKIES {
+    private:
+    public:
+    RGB_SEN RGB_Sensor;
+};
 
 #endif
